22ab5adda9834df1390de5895e8fea82f75a1c38
m4gie <steeven.regnault@epitech.eu>
2020-10-12 12:07:17 +0200

    feat: create home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,30 +5,18 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `M4gie - Portfolio`,
+        short_name: `m4gie`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#224984`,
+        theme_color: `#FFFFFF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const NotFoundPage = () => <Layout></Layout>
+
+export default NotFoundPage
